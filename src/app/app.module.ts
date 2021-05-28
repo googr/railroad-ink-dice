7d518cf4aad21d837e32cb1d47e3539fe12be552
Andrei_Rahozik <andrei.rahozik@netcracker.com>
2021-05-28 13:16:46 +0300

    fix revert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,15 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {DiceComponent} from './dice/dice.component';
 import {FormsModule} from '@angular/forms';
-import { DiceTrayComponent } from './dice-tray/dice-tray.component';
-import {RouterModule, Routes} from '@angular/router';
-import { RulesComponent } from './rules/rules.component';
-import {PdfViewerModule} from 'ng2-pdf-viewer';
-
-const routes: Routes = [
-  { path: '',   redirectTo: '/dice-tray', pathMatch: 'full' },
-  { path: 'dice-tray', component: DiceTrayComponent },
-  { path: 'rules', component: RulesComponent }
-];
 
 @NgModule({
   declarations: [
     AppComponent,
     DiceComponent,
-    DiceTrayComponent,
-    RulesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    PdfViewerModule,
-    RouterModule.forRoot(routes)
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
